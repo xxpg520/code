67b4d915a434a036535f375f0dbaaf02f86b883a
xiaobai <xxpg520@gamil.com>
2022-02-19 01:49:37 +0800

    2

--- number.py
+++ number.py
@@ -13,6 +13,7 @@ print(8/5)
 # 整除 小数点后不保留
 print(8//5)  
 
+
 # 连续运算符 先乘除后加减
 print(8+8*5)
 
@@ -22,3 +23,6 @@ print((8+8)*5)
 # 变量带入运算
 number = 8
 print(number * 5)
+
+# 取余数  8÷5=1……3 
+print(number % 5)
