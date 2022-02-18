53c5958cc69629cdf2b5227faabb6ba83e659379
xiaobai <xxpg520@gamil.com>
2022-02-19 01:08:04 +0800

    xiaobai

---
+++ string.py
@@ -0,0 +1,13 @@
+# 如何使用字串、字串的用法
+
+# 换行符 \n
+print("Hello \nMr.White")
+
+# 字串中有双引号  \"
+print("Hello\" Mr.White")
+
+# 两个字串相加
+print("Hello " + "Mr.White")
+
+phrase = "Hello "
+print(phrase + "Mr.White")
