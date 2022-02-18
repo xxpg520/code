513d15ab9bb758ba0420160c09be2b8708fc001b
xiaobai <xxpg520@gamil.com>
2022-02-19 01:14:09 +0800

    xiaobai

--- string.py
+++ string.py
@@ -11,3 +11,16 @@ print("Hello " + "Mr.White")
 
 phrase = "Hello "
 print(phrase + "Mr.White")
+
+# 全部变为小写字母函数  lower()
+phrase = "Hello Mr.White"
+print(phrase.lower())
+
+# 全部变为大写字母函数 upper()
+print(phrase.upper())
+
+# 判断是否全是大写，是则返回 True，否则返回 false
+print(phrase.isupper())
+
+# 判断是否全是小写，是则返回 True，否则返回 false
+print(phrase.islower())
