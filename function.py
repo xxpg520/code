d7ac0a20dbb8210a14671f15a6c0017549534094
xiaobai <xxpg520@gamil.com>
2022-02-19 04:04:12 +0800

    更新

---
+++ function.py
@@ -0,0 +1,15 @@
+""" 函数 function"""
+
+# 定义函数
+def hello(name,age):
+    print("hello" + name + "你今年" + str(age) +"岁")
+# 呼叫函数
+hello("小白",87)
+
+
+# 练习
+def add(numb1,numb2):
+    # print(numb1 + numb2)
+    return numb1 + numb2  # 返回值
+
+print(add(2,3))
