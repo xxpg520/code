5708b7dd2de87881c01b1385f7daef0010419564
xxpg520 <xxpg520@gmai.com>
2022-02-21 22:30:29 +0800

    1

--- if_statements.py
+++ if_statements.py
@@ -72,9 +72,9 @@ else:
 #     我给你1000元
 # 否则
 # 你给我100元
-score = 90
+score = 100
 rainy = True
-if score == 100 or not(rainy):
+if score != 100 or not(rainy):
     print("我给你1000元")
 else:
     print("你给我100元")
