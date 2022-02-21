cfed4e02301b2eb9848a599c0555aec4eb4ee48f
xxpg520 <85958347+xxpg520@users.noreply.github.com>
2022-02-21 15:29:44 +0800

    Update datatypes_and_variables.py

--- datatypes_and_variables.py
+++ datatypes_and_variables.py
@@ -1,9 +1,25 @@
-# 基本资料类型 & 变量
+# 基本资料形态 & 变数
+
+# # 字串 str
+# "小白你好"
+# '小白你好'
+#
+# # 数字
+# 70          int
+# -87         int
+# 160.5       float
+# 160.5555    float
+#
+# # 布林值
+# 是  True
+# 否  False
+
+name = "小黑"
+age = 87
+is_male = True
 
 
-print("有一个人叫小白")
-print("小白今年87岁")
-print("小白身高187公分")
-print("小白讨厌自己87岁")
-
-# 终于弄好了
+print("有一个人叫"+ name)
+print(name+"今年87岁")
+print(name+"身高187公分")
+print(name+"讨厌自己87岁")
