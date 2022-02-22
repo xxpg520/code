a949e59492edf5ec89067c287af73488ab2900f6
xiaobai <xxpg520@gamil.com>
2022-02-23 01:49:39 +0800

    修正错误

--- 循环 for.py
+++ 循环 for.py
@@ -53,8 +53,8 @@ for 变量 in 字串or列表:
 def power(base_num,pow_num):
     result = base_num
     for index in range(pow_num-1):
-        result = result + base_num
+        result = result * base_num
     return result
 
-print(power(2,5))
+print(power(4,2))
 
