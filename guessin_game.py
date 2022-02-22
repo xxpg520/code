ac2dd20bbafdbe8cae42f5cb36e2d7d7b3dff08e
xiaobai <xxpg@gmail.com>
2022-02-22 17:00:26 +0800

    1

--- guessin_game.py
+++ guessin_game.py
@@ -9,13 +9,15 @@ while guess != num and not(out_of_limit):
     guess_count += 1
     if guess_count < guess_limit:
         guess = int(input("请输入你猜的数字："))
-        guess_count += 1
         if guess > num:
             print("你猜的数字大了")
         elif guess < num:
             print("你猜的数字小了")
+    elif guess == num:
+        print("你猜对了")
     else:
         out_of_limit = True
 if out_of_limit:
     print("抱歉，你输了")
+
 print("游戏结束") 
