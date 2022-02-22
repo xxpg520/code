0065455981aa2e64a570c587569dc3ec378d56f4
xiaobai <xxpg520@gamil.com>
2022-02-22 22:12:39 +0800

    更新

--- guessin_game.py
+++ guessin_game.py
@@ -5,19 +5,17 @@ guess = None
 guess_count = 0
 guess_limit = 3
 out_of_limit = False
-while guess != num and not(out_of_limit):
+while guess != num and not(out_of_limit):  #设定循环条件 guess是否等于谜底 and  是否满足循环条件out
     guess_count += 1
-    if guess_count < guess_limit:
+    if guess_count <= guess_limit:
         guess = int(input("请输入你猜的数字："))
         if guess > num:
             print("你猜的数字大了")
         elif guess < num:
-            print("你猜的数字小了")
-    elif guess == num:
-        print("你猜对了")
+            print("你猜的数字小了")   
     else:
         out_of_limit = True
 if out_of_limit:
     print("抱歉，你输了")
-
-print("游戏结束") 
+else :
+    print("恭喜你赢了")
