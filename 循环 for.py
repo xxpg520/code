3057ae7478c27ee8c9755446a4fe5f9301e68dcb
xiaobai <xxpg520@gamil.com>
2022-02-22 23:48:32 +0800

    2

---
+++ 循环 for.py
@@ -0,0 +1,21 @@
+""" for 循环 """
+"""
+for 变量 in 字串or列表:
+    要重复执行的程序代码 """
+
+# for letter in "小白你好":
+#     print(letter)
+
+# for num in [0,1,2,3,4]:  
+#     print(num)
+
+# 下行用range()函数展示
+# for num in range(2,7): # 从2到7之间不包含7
+#     print(num)
+
+# 练习
+print(pow(2,6))  #2的5此
+
+def power(num1,num2):
+    result = num1
+    for num3 in range()
