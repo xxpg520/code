4b77241a07546bcac2fa50b0d75c03b6ced68fef
xiaobai <xxpg520@gamil.com>
2022-02-22 21:38:03 +0800

    1

--- datatypes_and_variables.py
+++ datatypes_and_variables.py
@@ -18,17 +18,7 @@ name = "小黑"
 age = 87
 is_male = True
 
-
-<<<<<<< HEAD
 print("有一个人叫小白")
 print("小白今年87岁")
 print("小白身高187公分")
 print("小白讨厌自己87岁")
-
-# 终于弄好了 
-=======
-print("有一个人叫"+ name)
-print(name+"今年87岁")
-print(name+"身高187公分")
-print(name+"讨厌自己87岁")
->>>>>>> ac2dd20bbafdbe8cae42f5cb36e2d7d7b3dff08e
