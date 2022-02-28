a23379af2da08af9e48e79c057e4ea8e69429792
xxpg520 <xxpg520@gmai.com>
2022-02-28 23:02:06 +0800

    1

--- xiaojiayu/10.py
+++ xiaojiayu/10.py
@@ -40,6 +40,6 @@ answer = input("请输入小甲鱼最想听的一句话:")
 while True:
     if answer == bingo:
         break
-    answe = input("抱歉，错了，请重新输入（答案正确才能退出游戏！ :)")
+    answer = input("抱歉，错了，请重新输入（答案正确才能退出游戏！ :)")
 print("哎呦，帅哦~")
 print("您真是小甲鱼肚子里的蛔虫啊^_^")
